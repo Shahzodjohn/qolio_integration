7cc51f630ce4d226a51828167aca201d0cf20eb9
shahzod.akhmedov <shahzod.akhmedov@bk.ru>
2024-05-22 10:17:25 +0300

    jivo log test

---
+++ WebHook/Controllers/WebhookController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using System.Text.Json;
+using WebHook.interfaces;
+
+namespace WebHook.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WebhookController : ControllerBase
+    {
+        public async Task<IActionResult> GetJivoData(JsonElement json)
+        {
+            Log.Information("New conversation...");
+
+            Console.WriteLine("New conversation...");
+            return Ok();
+            //var requestBody = await context.Request.ReadFromJsonAsync<JsonElement>();
+
+            //using (var serviceScope = app.Services.CreateScope())
+            //{
+            //    var services = serviceScope.ServiceProvider;
+
+            //    var myDependency = services.GetRequiredService<IServiceInterface>();
+
+            //    await myDependency.SaveToDb(requestBody);
+            //}
+        }
+    }
+}
